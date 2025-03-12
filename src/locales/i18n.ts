d35cf88ca74c07093c9f3203714075739f984374
hurole <1192163814@qq.com>
2025-03-12 22:26:30 +0800

    feat: add i18n and daisyui

---
+++ src/locales/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import enUS from './en-US/translation.json';
+import zhCN from './zh-CN/translation.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      'en-US': {
+        translation: enUS,
+      },
+      'zh-CN': {
+        translation: zhCN,
+      },
+    },
+  });
+
+export default i18n;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,15 @@
 import type { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  const { t, i18n } = useTranslation();
+  console.log(i18n.language);
+  return (
+    <div>
+      <button className="btn">按钮</button>
+      <span>{t('hello')}</span>
+    </div>
+  );
 };
 
 export default Login;
